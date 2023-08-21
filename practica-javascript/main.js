ff6aced1633b208771531793601b26d8c148305e
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-08-21 16:34:26 -0600

    Terminando introducción

---
+++ practica-javascript/main.js
@@ -0,0 +1,12 @@
+//alert('Hola')
+
+//variables
+//var, let, const
+let nombre = "Jesus Solis"
+console.log(nombre)
+
+let datos = document.querySelector('#datos')
+
+datos.innerHTML = `<h1>Caja de datos</h1>
+                    <h2>${nombre}</h2>`
+
